68370eb16c0f929fa78b320776e3bc90989fa938
Brady <brady.davenport@pm.me>
2022-05-23 21:48:00 -0600

    adds cache, refactors

--- modules/movies.js
+++ modules/movies.js
@@ -1,26 +1,44 @@
 'use strict';
 
 const axios = require('axios');
+let cache = require('./cache.js');
 
-async function getMovies (req, res, next) {
+async function getMovies(req, res, next) {
 
   let city_name = req.query.city_name;
+  const key = 'moviedb-' + city_name;
 
-  try {
-
-    let movieResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city_name}&include_adult=false`);
-    let movieData = movieResults.data.results;
-    let dataToSend = movieData.map(movie => new Movie(movie));
-    res.status(200).send(dataToSend);
-  } catch (e) {
-    next(e);
+  if (cache[key] && ((Date.now() - cache[key].timestamp) < (1000 * 60 * 60 * 24 * 30))) {
+    try {
+      console.log('Cache hit');
+      let movieResults = cache[city_name];
+      console.log(movieResults);
+      let movieData = movieResults.data;
+      let dataToSend = movieData.map(movie => new Movie(movie));
+      res.status(200).send(dataToSend);
+    } catch (e) {
+      next(e);
+    }
+  } else {
+    try {
+      console.log('Cache miss');
+      let movieResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city_name}&include_adult=false`);
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      cache[key].data = movieResults;
+      let movieData = movieResults.data.results;
+      let dataToSend = movieData.map(movie => new Movie(movie));
+      res.status(200).send(dataToSend);
+    } catch (e) {
+      next(e);
+    }
   }
 }
 
 class Movie {
   constructor(movie) {
-    this.image_url = 
-    this.posterpath = movie.posterpath;
+    this.image_url =
+      this.posterpath = movie.posterpath;
     this.title = movie.title;
     this.overview = movie.overview;
     this.releasedOn = movie.released_date;
--- modules/weather.js
+++ modules/weather.js
@@ -1,22 +1,35 @@
 'use strict';
 
 const axios = require('axios');
+let cache = require('./cache.js');
 
-async function getWeather(req, res, next) {
+function getWeather(lat, lon) {
 
-  let lat = req.query.lat;
-  let lon = req.query.lon;
+  const key = 'weather-' + lat + lon;
 
+  if (cache[key] && ((Date.now() - cache[key].timestamp) < (1000 * 60 * 60 * 24))) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=5`)
+      .then(response => parseWeather(response.data));
+  }
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
   try {
-    let weatherResults = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=5`);
-    let weatherData = weatherResults.data.data;
-    let dataToSend = weatherData.map(day => new Forecast(day));
-    res.send(dataToSend);
-
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Forecast(day);
+    });
+    return Promise.resolve(weatherSummaries);
   } catch (e) {
-    next(e);
+    return Promise.reject(e);
   }
 }
+
 
 class Forecast {
   constructor(day) {
@@ -29,3 +42,4 @@ class Forecast {
 }
 
 module.exports = getWeather;
+
