672aaa4fb8b56cee0b0acebe775eea478d6a29dd
Brady <brady.davenport@pm.me>
2022-05-28 15:09:12 -0600

    movies cache

--- modules/movies.js
+++ modules/movies.js
@@ -8,37 +8,29 @@ async function getMovies(req, res, next) {
   let city_name = req.query.city_name;
   const key = 'moviedb-' + city_name;
 
-  if (cache[key] && ((Date.now() - cache[key].timestamp) < (1000 * 60 * 60 * 24 * 30))) {
-    try {
+  try {
+    const cacheTime = 1000 * 60 * 60 * 24 * 30;
+    if (cache[key] && (Date.now() - cache[key].timestamp < cacheTime)) {
       console.log('Cache hit');
-      let movieResults = cache[city_name];
-      console.log(movieResults);
-      let movieData = movieResults.data;
-      let dataToSend = movieData.map(movie => new Movie(movie));
-      res.status(200).send(dataToSend);
-    } catch (e) {
-      next(e);
-    }
-  } else {
-    try {
+      res.status(200).send(cache[key].data);
+    } else {
       console.log('Cache miss');
       let movieResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city_name}&include_adult=false`);
-      cache[key] = {};
-      cache[key].timestamp = Date.now();
-      cache[key].data = movieResults;
-      let movieData = movieResults.data.results;
-      let dataToSend = movieData.map(movie => new Movie(movie));
-      res.status(200).send(dataToSend);
-    } catch (e) {
-      next(e);
+      let movieData = movieResults.data.results.map(movie => new Movie(movie));
+      cache[key] = {
+        data: movieData,
+        timestamp: Date.now()
+      };
+      res.status(200).send(movieData);
     }
+  } catch (e) {
+    next(e);
   }
 }
 
 class Movie {
   constructor(movie) {
-    this.image_url =
-      this.posterpath = movie.posterpath;
+    this.image_url = (movie.poster_path !== null) ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : `https://unsplash.com/photos/Dnkr_lmdKi8`;
     this.title = movie.title;
     this.overview = movie.overview;
     this.releasedOn = movie.released_date;
